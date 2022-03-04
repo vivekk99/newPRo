8d191923af153040eff22100fcf7153b953e7c7d
vivekk99 <80243113+vivekk99@users.noreply.github.com>
2022-03-04 13:42:46 +0530

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -24,12 +24,26 @@ exports.getProductDetails = async (req, res) => {
     try {
       const [data] = await products.getProductsByUser(id);
 
+      let house = data.reduce(function (n, person) {
+        return n + (person.category_name == 'house');
+    }, 0);
+    
+    let business = data.reduce(function (n, person) {
+        return n + (person.category_name == 'business');
+    }, 0);
+    
+    let personal = data.reduce(function (n, person) {
+        return n + (person.category_name == 'personal');
+    }, 0);
+const graph = [{category:"Personal",deviceCount:personal,color:'#F5AA00'},{category:"Business",deviceCount:business,color:'#00c569'},
+{category:"House",deviceCount:house,color:'#FF4081'}
+]
       res.json({
         status: "Success",
         statusCode: 200,
         data: data,
-        count:data.length
-        // message:message,
+        count:data.length,
+        graphData:graph,
       });
     } catch (error) {
       res.json({
