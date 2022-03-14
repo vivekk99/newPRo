731bdaa7f6496eb2eda2bdfac563da3c0c0066e6
vivekk99 <80243113+vivekk99@users.noreply.github.com>
2022-03-14 11:05:50 +0530

    Update productsModel.js

--- models/productsModel.js
+++ models/productsModel.js
@@ -5,9 +5,6 @@ exports.getProducts= async()=>{
     const data= await db.select('*').from('product_table').join('user_info','product_table.user_id','user_info.id').join('brands_details','product_table.brand_id','brands_details.id')
     .join('warrenty','product_table.warrenty_id','warrenty.id').join('unit_type','product_table.unit_id','unit_type.id').join('warrenty_types','product_table.warrenty_type_id','warrenty_types.id')
     .join('user_type','product_table.user_type_id','user_type.id').join('purchase_mode','product_table.purchase_mode_id','purchase_mode.id').join('payment_mode','product_table.payment_mode_id','payment_mode.id')
-
-    const data1 =await  db.select('*').from('product_table');
-    console.log('------------------------',data1);
    return [data]
 }
 
@@ -44,7 +41,7 @@ exports.getProductsByUser = async(id)=>{
   let datas =[];
   const data= await db.select('*').from('product_table').join('user_info','product_table.user_id','user_info.id').join('brands_details','product_table.brand_id','brands_details.id')
   .join('warrenty','product_table.warrenty_id','warrenty.id').join('warrenty_types','product_table.warrenty_type_id','warrenty_types.id').join('unit_type','product_table.unit_id','unit_type.id')
-  .join('user_type','product_table.user_type_id','user_type.id').join('purchase_mode','product_table.purchase_mode_id','purchase_mode.id').join('category','product_table.category_id','category.id').join('sub_category','product_table.sub_category_id','sub_category.id').join('payment_mode','product_table.payment_mode_id','payment_mode.id')
+  .join('user_type','product_table.user_type_id','user_type.id').join('purchase_mode','product_table.purchase_mode_id','purchase_mode.id').join('category','product_table.category_id','category.id').join('sub_category','product_table.sub_category_id','sub_category.id').join('payment_mode','product_table.payment_mode_id','payment_mode.id').orderBy('product_table.id', 'desc')
   .where({user_id:id})
 for (let index = 0; index < data.length; index++) {
     let values = data[index];
